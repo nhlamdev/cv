1e032a116b7d53bedcb75fb534c6801234cfbdcd
Hoàng Lâm <dev.nhlam@gmail.com>
2023-12-14 05:56:23 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import '@/styles/global.scss'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import createCache from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { SnackbarProvider } from 'notistack'
+import { RootContainer } from './container'
+
+const RootComponent = () => {
+  const emotionCache = createCache({ key: 'css' })
+
+  console.log(process.env)
+  return (
+    <React.StrictMode>
+      <CacheProvider value={emotionCache}>
+        <SnackbarProvider>
+          <RootContainer />
+        </SnackbarProvider>
+      </CacheProvider>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<RootComponent />)
---
+++ webpack.config.js
@@ -0,0 +1,187 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const webpack = require('webpack')
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+  const isAnalyze = Boolean(env?.analyze)
+
+  const config = {
+    entry: {
+      bundle: path.resolve(__dirname, 'src/index.tsx')
+      // doom: path.resolve(__dirname, 'src/dom/index.ts')
+    },
+    output: {
+      filename: 'js/[name].[contenthash].js', // output
+      chunkFilename: 'js/[id].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      clean: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash].css'
+      }),
+      new Dotenv({ path: isProduction ? '.env.production' : '.env.development' }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: '.',
+            filter: (name) => {
+              return !name.endsWith('index.html')
+            }
+          }
+        ]
+      }),
+      new ESLintPlugin({
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
+      })
+    ],
+    devtool: isProduction ? false : 'source-map',
+    mode: env.mode,
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`
+            }
+          }
+        }
+      },
+      runtimeChunk: {
+        name: 'runtime'
+      },
+      flagIncludedChunks: true,
+      minimize: true,
+      realContentHash: true,
+      removeEmptyChunks: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts)x?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: path.resolve(__dirname, 'dist/css/')
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: { sourceMap: !isProduction }
+            },
+            {
+              loader: 'sass-loader', // biên dịch sass sang css
+              options: { sourceMap: !isProduction }
+            },
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|wav|ico)$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'assets/fonts'
+            }
+          }
+        },
+        {
+          test: /\.(png|jpe?g|gif|)$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'assets/imgs'
+            }
+          }
+        },
+        {
+          test: /\.mp3$/i,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'assets/music'
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      },
+
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    devServer: {
+      compress: true,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+      port: 3001,
+      static: {
+        directory: path.join(__dirname, 'public'),
+        serveIndex: true,
+        watch: true
+      },
+      watchFiles: {
+        paths: ['src/**/*.*'],
+        options: {
+          usePolling: true
+        }
+      }
+    }
+  }
+
+  if (isProduction) {
+    config.plugins = [
+      ...config.plugins,
+      new webpack.ProgressPlugin(),
+
+      new CompressionPlugin({
+        test: /\.(css|js)$/,
+        algorithm: 'brotliCompress'
+      }),
+      new CleanWebpackPlugin()
+    ]
+    if (isAnalyze) {
+      config.plugins = [...config.plugins, new BundleAnalyzerPlugin()]
+    }
+    config.optimization = {
+      minimizer: [`...`, new CssMinimizerPlugin()]
+    }
+  }
+
+  return config
+}
